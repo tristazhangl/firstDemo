b2094196a8219263fecd53f4c772ae4d8f64a4de
trista <trista.zhang@celnet.com.cn>
2020-02-16 19:35:51 +0800

    first push

---
+++ public/static/js/SFDCApi.js
@@ -0,0 +1,23 @@
+﻿//实例化forcetk
+export function SFDCApi() {
+
+  window.client = new forcetk.Client();
+//console.log("SFDCAPI",context);
+//设置SessionToken
+  var InstanceUrl;
+// if (context.EnableReverseProxy) {
+//     InstanceUrl = context.ReverseProxyUrl;
+// }
+// else {
+//     InstanceUrl = context.InstanceUrl;
+// }
+  InstanceUrl = context.InstanceUrl;
+  client.setSessionToken(context.SessionId, context.ApiVersion, InstanceUrl);
+//client.setSessionToken(context.SessionId, context.ApiVersion, "https://p.veevlink.com");
+
+//设置RefreshTokenProxy，AccessToken失效后会调用RefreshTokenProxy刷新AccessToken
+  console.log(context);
+//client.setRefreshTokenProxy(context.OpenId, context.AppId, context.RefreshTokenProxyUrl,context.SFDCUserId);
+  client.setRefreshTokenProxy(context.OpenId, context.WEQYUserId, context.AppId, context.RefreshTokenProxyUrl, context.SFDCUserId);
+
+}
